2e52fe45ed018e3a70e1c844d2e0422eb576bb38
Austin West <korruptedanomaly@gmail.com>
2019-06-17 14:21:18 -0600

    Storyboard set up, added SwitchTableViewCell

---
+++ myAlarm/Models/CustomCell/SwitchTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SwitchTableViewCell.swift
+//  myAlarm
+//
+//  Created by Austin West on 6/17/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+
+}
